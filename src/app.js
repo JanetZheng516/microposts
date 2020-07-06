c9c0cdced4579cd036b9c07b7ccf2e5afe28eaa0
Ziwan Zheng <janet@pop-os.localdomain>
2020-07-06 15:57:06 +0200

    all files

---
+++ src/app.js
@@ -0,0 +1,142 @@
+// const greeting = 'Hello World';
+// console.log(greeting);
+
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+
+// getData('https://jsonplaceholder.typicode.com/posts');
+
+
+
+// CommonJS Module Syntax
+// const person = require('./mymodule1');
+
+// ES2015 Module
+// import { person, sayHello } from './mymodule2';
+// import greeting from './mymodule2';
+
+// // console.log(person.name);
+
+// // console.log(sayHello());
+
+// console.log(greeting);
+
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click',cancelEdit);
+
+
+// Get posts
+function getPosts() {
+    http.get('http://localhost:3000/post')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+// Submit posts
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+    const id = document.querySelector('#id').value;
+
+    const data = {
+        title,
+        body,
+    }
+
+    // Validate input
+    if(title === '' || body === '') {
+        ui.showAlert('Please fill in all fields', 'alert alert-danger');
+    } else {
+        // Check for ID
+        if(id === '') {
+            // Creat Post
+            http.post('http://localhost:3000/post', data)
+            .then(data => {
+            ui.showAlert('Post added', 'alert alert-success');
+            ui.clearFields();
+            getPosts();
+        })
+        .catch(err => console.log(err));
+
+        } else {
+            // Update Post
+            http.put(`http://localhost:3000/post/${id}`, data)
+            .then(data => {
+            ui.showAlert('Post updated', 'alert alert-success');
+            ui.changeFormState('add');
+            getPosts();
+       
+        })
+        .catch(err => console.log(err));
+     }
+    }
+}
+
+
+    
+
+
+// Delete Post
+function deletePost(e) {
+    e.preventDefault();
+
+    if(e.target.parentElement.classList.contains('delete')) {
+        const id = e.target.parentElement.dataset.id;
+        if(confirm('Are you sure?')){
+            http.delete(`http://localhost:3000/post/${id}`)
+            .then(data => {
+                ui.showAlert('Post deleted','alert alert-success');
+                getPosts();
+            })
+            .catch(err => console.log(err));
+        }
+
+    }
+}
+
+// Enable Edit State
+function enableEdit(e) {
+    e.preventDefault();
+
+    if(e.target.parentElement.classList.contains('edit')) {
+        const id = e.target.parentElement.dataset.id;
+        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        const body = e.target.parentElement.previousElementSibling.textContent;
+       
+        const data = {
+            id,
+            title,
+            body
+        }
+
+        // Fill form with current post
+        ui.fillForm(data);
+    }
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+    if(e.target.classList.contains('post-cancel')) {
+        ui.changeFormState('add');
+    }
+
+    e.preventDefault();
+}
